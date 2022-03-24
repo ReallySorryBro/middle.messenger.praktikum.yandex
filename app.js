66cf9858634f1f2bb573194a308ba05869ca52c1
Сорокина Полина Евгеньевна <PESorokina@sberbank.ru>
2022-03-24 17:54:10 +0300

    updated repo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'dist/index.hbs'));
+});
+
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
