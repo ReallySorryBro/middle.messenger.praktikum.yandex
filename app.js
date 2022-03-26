c5ee7fcc38ad2bf56a894db5a666b73f4057d1c4
Сорокина Полина Евгеньевна <PESorokina@sberbank.ru>
2022-03-26 12:06:51 +0300

    small fixes

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
-router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.hbs'));
-});
 
 app.listen(process.env.port || 3000);
 
